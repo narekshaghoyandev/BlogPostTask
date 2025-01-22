19681a5373810acfae140ab79c5b666da4f5ac2f
narekshaghoyandev <narekshagoyan7@gmail.com>
2025-01-22 13:22:49 +0400

    update database file

--- config/database.js
+++ config/database.js
@@ -3,7 +3,18 @@ require('dotenv').config();
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: 'mysql',
+    logging: false, // Отключить SQL-логи в консоли (опционально)
 });
+
+(async () => {
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
+})();
 
 module.exports = sequelize;
